63a281771fb2a6ea3825b6c024a46feaf3dc2b9f
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-05-28 18:03:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -595,3 +595,8 @@ tool_init_on_cmd = [
     ["amass enum -d ", ""]
 ]
 
+tools = [
+    ["wapiti"], ["whatweb"], ["nmap"], ["golismero"], ["host"], ["wget"], ["uniscan"], ["wafw00f"], ["dirb"],
+    ["davtest"], ["theHarvester"], ["xsser"], ["dnsrecon"], ["fierce"], ["dnswalk"], ["whois"], ["sslyze"], ["lbd"],
+    ["golismero"], ["dnsenum"], ["dmitry"], ["davtest"], ["nikto"], ["dnsmap"], ["amass"]
+]
